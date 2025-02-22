33cbf622c2060293c59f4342ca2e2f6f8ad2f630
tungphan1011 <gametungphan10@gmail.com>
2025-02-22 10:37:25 +0700

    add regiser model

---
+++ ZenGarden/ZenGarden.API/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ZenGarden.API.Models
+{
+    public class RegisterModel
+    {
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+    }
+}
