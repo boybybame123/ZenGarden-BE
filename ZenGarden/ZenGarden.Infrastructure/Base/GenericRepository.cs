ccb6af6223114f436e1a73344267f1200d2bc7a8
tungphan1011 <gametungphan10@gmail.com>
2025-02-22 10:47:04 +0700

    model code

---
+++ ZenGarden/ZenGarden.Infrastructure/Base/GenericRepository.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ZenGarden.Infrastructure.Persistence;
+
+namespace ZenGarden.Infrastructure.Base
+{
+    public class GenericRepository<T> where T : class
+    {
+        protected ZenGardenContext _context;
+
+        public GenericRepository()
+        {
+            _context ??= new ZenGardenContext();
+        }
+
+        public GenericRepository(ZenGardenContext context)
+        {
+            _context = context;
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public virtual T Get(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().FirstOrDefault(expression);
+        }
+        public virtual List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression).ToList();
+        }
+
+        public void Create(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<int> CreateAsync(T entity)
+        {
+            _context.Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public async Task<int> UpdateAsync(T entity)
+        {
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+
+            return await _context.SaveChangesAsync();
+        }
+
+        public bool Remove(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public async Task<bool> RemoveAsync(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public T GetById(string code)
+        {
+            return _context.Set<T>().Find(code);
+        }
+
+        public async Task<T> GetByIdAsync(string code)
+        {
+            return await _context.Set<T>().FindAsync(code);
+        }
+
+        public T GetById(Guid code)
+        {
+            return _context.Set<T>().Find(code);
+        }
+
+        public async Task<T> GetByIdAsync(Guid code)
+        {
+            return await _context.Set<T>().FindAsync(code);
+        }
+
+        #region Separating asign entity and save operators        
+
+        public void PrepareCreate(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        public void PrepareUpdate(T entity)
+        {
+            var tracker = _context.Attach(entity);
+            tracker.State = EntityState.Modified;
+        }
+
+        public void PrepareRemove(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        #endregion Separating asign entity and save operators
+        public async Task<int> Count()
+        {
+            return await _context.Set<T>().CountAsync();
+        }
+
+        public async Task<int> FindEmptyPositionWithBinarySearch(List<T> list, int low, int high, string entityName, string entityIndex)
+        {
+            var mid = (low + high) / 2;
+            var Id = $"{entityName}{mid.ToString("D4")}";
+            var entity = await _context.Set<T>()
+                .Where(e => EF.Property<string>(e, entityIndex) == Id)
+                .FirstOrDefaultAsync();
+            if (entity is null)
+            {
+                return mid;
+            }
+            else
+            {
+                var index = list.IndexOf(entity) + 1;
+                if (index < mid)
+                {
+                    return await FindEmptyPositionWithBinarySearch(list, low, mid, entityName, entityIndex);
+                }
+                else
+                {
+                    return await FindEmptyPositionWithBinarySearch(list, mid, high, entityName, entityIndex);
+                }
+            }
+        }
+    }
+}
