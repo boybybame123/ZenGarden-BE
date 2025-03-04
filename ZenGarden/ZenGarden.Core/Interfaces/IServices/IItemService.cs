88e43fa5720fdce4b68b27c653938be36abd4738
tungphan1011 <gametungphan10@gmail.com>
2025-03-04 19:37:18 +0700

    update item

---
+++ ZenGarden/ZenGarden.Core/Interfaces/IServices/IItemService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenGarden.Domain.DTOs;
+using ZenGarden.Domain.Entities;
+
+namespace ZenGarden.Core.Interfaces.IServices
+{
+    public interface IItemService
+    {
+        Task<List<ItemDto>> GetAllItemsAsync();
+        Task<Item?> GetItemByIdAsync(int ItemId);
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(ItemDto item);
+        Task DeleteItemAsync(int ItemId);
+    }
+}
---
+++ ZenGarden/ZenGarden.Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenGarden.Core.Interfaces.IRepositories;
+using ZenGarden.Domain.Entities;
+using ZenGarden.Infrastructure.Persistence;
+
+namespace ZenGarden.Infrastructure.Repositories
+{
+    public class ItemRepository(ZenGardenContext context) : GenericRepository<Item>(context), IItemRepository
+    {
+        private readonly ZenGardenContext _context = context;
+        public async Task<List<Item>> GetAllItemAsync()
+        {
+            return await _context.Item.Include(u => u.ItemDetail).ToListAsync();
+        }
+
+    }
+}
