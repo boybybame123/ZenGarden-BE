ed481def148ac0d14653b29f80b4223ea0ac243f
tungphan1011 <gametungphan10@gmail.com>
2025-03-11 20:34:00 +0700

    add tree

---
+++ ZenGarden/ZenGarden.Domain/DTOs/TreeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenGarden.Domain.Entities;
+
+namespace ZenGarden.Domain.DTOs
+{
+    public class TreeDto
+    {
+        public int TreeId { get; set; }
+        public string Name { get; set; }
+        public string Rarity { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+
+
+    }
+}
