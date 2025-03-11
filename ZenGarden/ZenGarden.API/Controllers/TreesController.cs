df143912da3755b66dd66e1a7ea17445b632b592
tungphan1011 <gametungphan10@gmail.com>
2025-03-11 13:38:11 +0700

    update controller

---
+++ ZenGarden/ZenGarden.API/Controllers/TreesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZenGarden.Domain.Entities;
+using ZenGarden.Infrastructure.Persistence;
+
+namespace ZenGarden.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreesController : ControllerBase
+    {
+        private readonly ZenGardenContext _context;
+
+        public TreesController(ZenGardenContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Trees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tree>>> GetTree()
+        {
+            return await _context.Tree.ToListAsync();
+        }
+
+        // GET: api/Trees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tree>> GetTree(int id)
+        {
+            var tree = await _context.Tree.FindAsync(id);
+
+            if (tree == null)
+            {
+                return NotFound();
+            }
+
+            return tree;
+        }
+
+        // PUT: api/Trees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTree(int id, Tree tree)
+        {
+            if (id != tree.TreeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tree).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TreeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Trees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tree>> PostTree(Tree tree)
+        {
+            _context.Tree.Add(tree);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTree", new { id = tree.TreeId }, tree);
+        }
+
+        // DELETE: api/Trees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTree(int id)
+        {
+            var tree = await _context.Tree.FindAsync(id);
+            if (tree == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tree.Remove(tree);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TreeExists(int id)
+        {
+            return _context.Tree.Any(e => e.TreeId == id);
+        }
+    }
+}
---
+++ ZenGarden/ZenGarden.API/Controllers/UserXpLogsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZenGarden.Domain.Entities;
+using ZenGarden.Infrastructure.Persistence;
+
+namespace ZenGarden.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserXpLogsController : ControllerBase
+    {
+        private readonly ZenGardenContext _context;
+
+        public UserXpLogsController(ZenGardenContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserXpLogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserXpLog>>> GetUserXpLog()
+        {
+            return await _context.UserXpLog.ToListAsync();
+        }
+
+        // GET: api/UserXpLogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserXpLog>> GetUserXpLog(int id)
+        {
+            var userXpLog = await _context.UserXpLog.FindAsync(id);
+
+            if (userXpLog == null)
+            {
+                return NotFound();
+            }
+
+            return userXpLog;
+        }
+
+        // PUT: api/UserXpLogs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserXpLog(int id, UserXpLog userXpLog)
+        {
+            if (id != userXpLog.LogId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userXpLog).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserXpLogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserXpLogs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserXpLog>> PostUserXpLog(UserXpLog userXpLog)
+        {
+            _context.UserXpLog.Add(userXpLog);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserXpLog", new { id = userXpLog.LogId }, userXpLog);
+        }
+
+        // DELETE: api/UserXpLogs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserXpLog(int id)
+        {
+            var userXpLog = await _context.UserXpLog.FindAsync(id);
+            if (userXpLog == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserXpLog.Remove(userXpLog);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserXpLogExists(int id)
+        {
+            return _context.UserXpLog.Any(e => e.LogId == id);
+        }
+    }
+}
