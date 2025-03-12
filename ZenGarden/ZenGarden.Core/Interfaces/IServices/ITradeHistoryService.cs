d846ceb2500064612fdcee9c312f61c24b20b38b
tungphan1011 <gametungphan10@gmail.com>
2025-03-12 12:28:17 +0700

    history and db

---
+++ ZenGarden/ZenGarden.Core/Interfaces/IServices/ITradeHistoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenGarden.Core.Interfaces.IRepositories;
+using ZenGarden.Domain.Entities;
+
+namespace ZenGarden.Core.Interfaces.IServices;
+
+public interface ITradeHistoryService
+{
+    Task<List<TradeHistory>> GetTradeHistoryAsync();
+    Task<TradeHistory> GetTradeHistoryByIdAsync(int id);
+    Task<TradeHistory> CreateTradeHistoryAsync(TradeHistory tradeHistory);
+    Task<TradeHistory> UpdateTradeHistoryAsync(TradeHistory tradeHistory);
+    Task<TradeHistory> DeleteTradeHistoryAsync(int id);
+}
