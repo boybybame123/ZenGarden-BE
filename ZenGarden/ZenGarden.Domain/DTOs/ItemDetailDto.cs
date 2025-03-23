b66d322481ecec33777f3bf750e3b02684044666
tungphan1011 <gametungphan10@gmail.com>
2025-03-23 23:46:04 +0700

    fix db

--- ZenGarden/ZenGarden.Domain/DTOs/ItemDetailDto.cs
+++ ZenGarden/ZenGarden.Domain/DTOs/ItemDetailDto.cs
@@ -17,4 +17,10 @@ public class ItemDetailDto
     public string Effect { get; set; } // Chứa JSON hiệu ứng
 
     public int? Duration { get; set; } // Thời gian hiệu lực (giây), NULL nếu vĩnh viễn
+
+    public int Sold { get; set; } // Số lần bán 
+
+    public bool IsUnique { get; set; } = false; // TRUE nếu chỉ mua 1 lần
+
+    public int? MonthlyPurchaseLimit { get; set; } // Thời gian hiệu lực (giây), NULL nếu vĩnh viễn
 }
