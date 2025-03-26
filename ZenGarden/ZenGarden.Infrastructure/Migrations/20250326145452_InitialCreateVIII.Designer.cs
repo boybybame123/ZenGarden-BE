8d1f7bf30a58a925d242526fb70068576d0243ca
tungphan1011 <gametungphan10@gmail.com>
2025-03-26 21:58:00 +0700

    fix code

--- ZenGarden/ZenGarden.Infrastructure/Migrations/20250326145452_InitialCreateVIII.Designer.cs
+++ ZenGarden/ZenGarden.Infrastructure/Migrations/20250326145452_InitialCreateVIII.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZenGarden.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using ZenGarden.Infrastructure.Persistence;
 namespace ZenGarden.Infrastructure.Migrations
 {
     [DbContext(typeof(ZenGardenContext))]
-    partial class ZenGardenContextModelSnapshot : ModelSnapshot
+    [Migration("20250326145452_InitialCreateVIII")]
+    partial class InitialCreateVIII
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1137,7 +1140,7 @@ namespace ZenGarden.Infrastructure.Migrations
                     b.Property<int?>("TreeStatus")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
-                        .HasDefaultValue(0);
+                        .HasDefaultValue(1);
 
                     b.Property<DateTime>("UpdatedAt")
                         .ValueGeneratedOnAddOrUpdate()
--- ZenGarden/ZenGarden.Infrastructure/Persistence/ZenGardenContext.cs
+++ ZenGarden/ZenGarden.Infrastructure/Persistence/ZenGardenContext.cs
@@ -949,7 +949,7 @@ public class ZenGardenContext : DbContext
 
             entity.Property(e => e.TreeStatus)
                 .HasConversion<int>()
-                .HasDefaultValue(TreeStatus.Growing);
+                .HasDefaultValue(TreeStatus.Seed);
 
             entity.Property(e => e.CreatedAt)
                 .HasColumnType("timestamp")
