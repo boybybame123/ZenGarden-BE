e99ebe0ffd8d2303a277a730d1cf3af0fc678e52
truonghoang <boybybame@gmail.com>
2025-03-27 10:45:46 +0700

    fix: level UserTree

--- ZenGarden/ZenGarden.Core/Services/UserXpConfigService.cs
+++ ZenGarden/ZenGarden.Core/Services/UserXpConfigService.cs
@@ -7,7 +7,7 @@ using ZenGarden.Domain.Entities;
 namespace ZenGarden.Core.Services;
 
 public class UserXpConfigService(
-    IUserXpConfigRepository UserXpConfigRepository,
+    IUserXpConfigRepository userXpConfigRepository,
     IUnitOfWork unitOfWork,
     IMapper mapper) : IUserXpConfigService
 {
@@ -15,42 +15,42 @@ public class UserXpConfigService(
     {
         var newUserXp = mapper.Map<UserXpConfig>(UserXpConfig);
 
-        await UserXpConfigRepository.CreateAsync(newUserXp);
+        await userXpConfigRepository.CreateAsync(newUserXp);
         if (await unitOfWork.CommitAsync() == 0)
             throw new InvalidOperationException("Failed to create item.");
     }
 
     public async Task DeleteUserXpConfigAsync(int UserXpConfigId)
     {
-        var i = await UserXpConfigRepository.GetByIdAsync(UserXpConfigId);
+        var i = await userXpConfigRepository.GetByIdAsync(UserXpConfigId);
 
         if (i == null)
             throw new KeyNotFoundException($"Item with ID {UserXpConfigId} not found.");
-        await UserXpConfigRepository.RemoveAsync(i);
+        await userXpConfigRepository.RemoveAsync(i);
     }
 
     public async Task<UserXpConfig?> GetUserXpConfigByIdAsync(int UserXpConfigId)
     {
-        var i = await UserXpConfigRepository.GetByIdAsync(UserXpConfigId);
+        var i = await userXpConfigRepository.GetByIdAsync(UserXpConfigId);
         return i;
     }
 
     public async Task UpdateUserXpConfigAsync(UserXpConfigDto UserXpConfig)
     {
-        var i = await UserXpConfigRepository.GetByIdAsync(UserXpConfig.LevelId);
+        var i = await userXpConfigRepository.GetByIdAsync(UserXpConfig.LevelId);
 
         if (i == null)
             throw new KeyNotFoundException($"Item with ID {UserXpConfig.LevelId} not found.");
         if (i.XpThreshold != null && i.XpThreshold >= 0) i.XpThreshold = UserXpConfig.XpThreshold;
 
-        UserXpConfigRepository.Update(i);
+        userXpConfigRepository.Update(i);
         if (await unitOfWork.CommitAsync() == 0)
             throw new InvalidOperationException("Failed to update item.");
     }
 
     public async Task<List<UserXpConfigDto>> GetAllUserXpConfigsAsync()
     {
-        var i = await UserXpConfigRepository.GetAllAsync();
+        var i = await userXpConfigRepository.GetAllAsync();
         try
         {
             var a = mapper.Map<List<UserXpConfigDto>>(i);
