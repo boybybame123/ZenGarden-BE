2e8834e5723dce389cdc2f7c680c162bb7d54574
tungphan1011 <gametungphan10@gmail.com>
2025-04-01 23:00:00 +0700

    fix code

--- ZenGarden/ZenGarden.API/Program.cs
+++ ZenGarden/ZenGarden.API/Program.cs
@@ -226,10 +226,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseDeveloperExceptionPage();
-app.UseSwagger(c =>
-{
-    c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
-});
+//app.UseSwagger(c =>
+//{
+//    c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
+//});
+app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
