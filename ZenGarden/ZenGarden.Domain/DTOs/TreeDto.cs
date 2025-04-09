aa9a116e0331806050467447a8900cc7129cf0a2
tungphan1011 <gametungphan10@gmail.com>
2025-04-09 11:46:39 +0700

    add  Description  in tree

--- ZenGarden/ZenGarden.Domain/DTOs/TreeDto.cs
+++ ZenGarden/ZenGarden.Domain/DTOs/TreeDto.cs
@@ -4,6 +4,7 @@ public class TreeDto
 {
     public int TreeId { get; set; }
     public string? Name { get; set; }
+    public string? Description { get; set; }
     public string? Rarity { get; set; }
     public string? ImageUrl { get; set; }
     public DateTime CreatedAt { get; set; }
