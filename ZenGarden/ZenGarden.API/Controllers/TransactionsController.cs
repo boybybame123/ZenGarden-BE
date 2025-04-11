894d6d34ce8e7f298eafb87b96320ebb5d6c9168
tungphan1011 <gametungphan10@gmail.com>
2025-04-11 11:07:28 +0700

    fix code

--- ZenGarden/ZenGarden.API/Controllers/TransactionsController.cs
+++ ZenGarden/ZenGarden.API/Controllers/TransactionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ZenGarden.Core.Interfaces.IServices;
 using ZenGarden.Domain.Entities;
 using ZenGarden.Infrastructure.Persistence;
 
@@ -7,82 +8,24 @@ namespace ZenGarden.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class TransactionsController : ControllerBase
+public class TransactionsController(ITransactionsService transactionsService) : ControllerBase
 {
-    private readonly ZenGardenContext _context;
-
-    public TransactionsController(ZenGardenContext context)
+    [HttpGet("user/{userId}")]
+    public async Task<IActionResult> GetAllTransactionsByUserId(int userId)
     {
-        _context = context;
-    }
-
-    // GET: api/Transactions
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Transactions>>> GetTransactions()
-    {
-        return await _context.Transactions.ToListAsync();
-    }
-
-    // GET: api/Transactions/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Transactions>> GetTransactions(int id)
-    {
-        var transactions = await _context.Transactions.FindAsync(id);
-
-        if (transactions == null) return NotFound();
-
-        return transactions;
-    }
-
-    // PUT: api/Transactions/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutTransactions(int id, Transactions transactions)
-    {
-        if (id != transactions.TransactionId) return BadRequest();
-
-        _context.Entry(transactions).State = EntityState.Modified;
-
         try
         {
-            await _context.SaveChangesAsync();
+            var transactions = await transactionsService.GetAllTransactionsByUserIdAsync(userId);
+            return Ok(transactions);
         }
-        catch (DbUpdateConcurrencyException)
+        catch (KeyNotFoundException ex)
         {
-            if (!TransactionsExists(id)) return NotFound();
-
-            throw;
+            return NotFound(ex.Message);
         }
-
-        return NoContent();
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 
-    // POST: api/Transactions
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPost]
-    public async Task<ActionResult<Transactions>> PostTransactions(Transactions transactions)
-    {
-        _context.Transactions.Add(transactions);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction("GetTransactions", new { id = transactions.TransactionId }, transactions);
-    }
-
-    // DELETE: api/Transactions/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteTransactions(int id)
-    {
-        var transactions = await _context.Transactions.FindAsync(id);
-        if (transactions == null) return NotFound();
-
-        _context.Transactions.Remove(transactions);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-    }
-
-    private bool TransactionsExists(int id)
-    {
-        return _context.Transactions.Any(e => e.TransactionId == id);
-    }
 }
