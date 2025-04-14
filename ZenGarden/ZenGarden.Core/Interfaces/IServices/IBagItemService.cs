d42dcb1bdf9194582fcf67b43ec1972af9a96bf0
tungphan1011 <gametungphan10@gmail.com>
2025-04-14 22:53:24 +0700

    fix

--- ZenGarden/ZenGarden.Core/Interfaces/IServices/IBagItemService.cs
+++ ZenGarden/ZenGarden.Core/Interfaces/IServices/IBagItemService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZenGarden.Domain.DTOs;
 using ZenGarden.Domain.Entities;
 
 namespace ZenGarden.Core.Interfaces.IServices
 {
     public interface IBagItemService
     {
-        Task<List<BagItem>?> GetListItemsByBagIdAsync(int bagId);
+        Task<List<BagItemDto>?> GetListItemsByBagIdAsync(int bagId);
     }
 }
--- ZenGarden/ZenGarden.Core/Services/BagItemService.cs
+++ ZenGarden/ZenGarden.Core/Services/BagItemService.cs
@@ -1,26 +1,51 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ZenGarden.Core.Interfaces.IRepositories;
 using ZenGarden.Core.Interfaces.IServices;
+using ZenGarden.Domain.DTOs;
 using ZenGarden.Domain.Entities;
 
 namespace ZenGarden.Core.Services
 {
-    public class BagItemService(IBagItemRepository bagItemRepository, IUnitOfWork unitOfWork) : IBagItemService
+    public class BagItemService : IBagItemService
     {
-        public async  Task<List<BagItem>?> GetListItemsByBagIdAsync(int bagId)
+        private readonly IBagItemRepository _bagItemRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IRedisService _redisService;
+
+        public BagItemService(IBagItemRepository bagItemRepository, IUnitOfWork unitOfWork, IMapper mapper, IRedisService redisService)
         {
-            var bagItems = await bagItemRepository.GetBagItemsByBagIdAsync(bagId);
+            _bagItemRepository = bagItemRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _redisService = redisService;
+        }
+
+        public async Task<List<BagItemDto>?> GetListItemsByBagIdAsync(int bagId)
+        {
+            var cacheKey = $"BagItems_{bagId}";
+            var cachedBagItems = await _redisService.GetAsync<List<BagItemDto>>(cacheKey);
+
+            if (cachedBagItems != null)
+            {
+                return cachedBagItems;
+            }
+
+            var bagItems = await _bagItemRepository.GetBagItemsByBagIdAsync(bagId);
             if (bagItems == null || !bagItems.Any())
             {
                 throw new KeyNotFoundException($"No items found for bag with ID {bagId}.");
             }
-            return bagItems;
+
+            var bagItemDtos = _mapper.Map<List<BagItemDto>>(bagItems);
+            await _redisService.SetAsync(cacheKey, bagItemDtos, TimeSpan.FromHours(1));
+
+            return bagItemDtos;
         }
-
-
     }
 }
