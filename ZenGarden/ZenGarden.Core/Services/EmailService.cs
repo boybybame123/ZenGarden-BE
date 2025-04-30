3c93328a3a775b06716aa9cb3ce7ac72902f8be9
truonghoang <boybybame@gmail.com>
2025-04-30 12:40:45 +0700

    fix: send email

--- ZenGarden/ZenGarden.Core/Services/EmailService.cs
+++ ZenGarden/ZenGarden.Core/Services/EmailService.cs
@@ -1,35 +1,32 @@
-using System.Net;
-using System.Net.Mail;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
 using ZenGarden.Core.Interfaces.IServices;
 
 namespace ZenGarden.Core.Services;
 
-public class EmailService : IEmailService
+public class EmailService(IConfiguration configuration) : IEmailService
 {
-    private readonly SmtpClient _smtpClient = new("smtp.gmail.com")
+    public async Task SendOtpEmailAsync(string email, string otp)
     {
-        Port = 587,
-        Credentials = new NetworkCredential("zengardensp25se111@gmail.com", "kmiultfbypxnqxqu"),
-        EnableSsl = true
-    };
-
-    public Task SendOtpEmailAsync(string email, string otp)
-    {
-        return SendEmailAsync(email, "Your ZenGarden OTP Code",
+        await SendEmailAsync(email, "Your ZenGarden OTP Code", 
             $"Your OTP code is: <b>{otp}</b>. It will expire in 5 minutes.");
     }
 
     private async Task SendEmailAsync(string email, string subject, string body)
     {
-        var mailMessage = new MailMessage
-        {
-            From = new MailAddress("zengardensp25se111@gmail.com"),
-            Subject = subject,
-            Body = body,
-            IsBodyHtml = true
-        };
-        mailMessage.To.Add(email);
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress("ZenGarden", configuration["EmailSettings:Username"]));
+        emailMessage.To.Add(new MailboxAddress("", email));
+        emailMessage.Subject = subject;
 
-        await _smtpClient.SendMailAsync(mailMessage);
+        var bodyBuilder = new BodyBuilder { HtmlBody = body };
+        emailMessage.Body = bodyBuilder.ToMessageBody();
+
+        using var smtpClient = new SmtpClient();
+        await smtpClient.ConnectAsync(configuration["EmailSettings:SmtpServer"], int.Parse(configuration["EmailSettings:Port"] ?? string.Empty), false);
+        await smtpClient.AuthenticateAsync(configuration["EmailSettings:Username"], configuration["EmailSettings:Password"]);
+        await smtpClient.SendAsync(emailMessage);
+        await smtpClient.DisconnectAsync(true);
     }
 }
