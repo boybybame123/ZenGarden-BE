e0431fc178aa0364c180653fe5610ec71743b911
tungphan1011 <gametungphan10@gmail.com>
2025-05-31 20:00:49 +0700

    fix code

--- ZenGarden/ZenGarden.Core/Interfaces/IServices/INotificationService.cs
+++ ZenGarden/ZenGarden.Core/Interfaces/IServices/INotificationService.cs
@@ -1,7 +1,10 @@
-﻿namespace ZenGarden.Core.Interfaces.IServices;
+﻿using ZenGarden.Domain.Entities;
+
+namespace ZenGarden.Core.Interfaces.IServices;
 
 public interface INotificationService
 {
+    Task<List<Notification>> GetNotificationsByUserIdAsync(int userId);
     Task PushNotificationAsync(int userId, string title, string content);
     Task PushNotificationToAllAsync(string title, string content);
 }
--- ZenGarden/ZenGarden.Infrastructure/Repositories/NotificationRepository.cs
+++ ZenGarden/ZenGarden.Infrastructure/Repositories/NotificationRepository.cs
@@ -8,4 +8,9 @@ namespace ZenGarden.Infrastructure.Repositories;
 public class NotificationRepository(ZenGardenContext context, IRedisService redisService)
     : GenericRepository<Notification>(context, redisService), INotificationRepository
 {
+    // No additional methods needed, inherits from GenericRepository
+    public async Task<List<Notification>> GetByUserIdAsync(int userId)
+    {
+        return await GetListAsync(n => n.UserId == userId || n.UserId == null);
+    }
 }
